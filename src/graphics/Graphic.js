93860a03a2b7f4e30c90112b9d65ceebb4b2c832
Brian Hayes <bjoeyhayes@gmail.com>
2019-01-02 21:57:56 -0500

    stuff is fixed... yay

--- src/graphics/Graphic.js
+++ src/graphics/Graphic.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Graphic = (props) => {
+import styles from './Graphic.module.scss';
 
+const Graphic = (props) => {
   const {
     // min-x, min-y, width, height
     viewBox,
@@ -13,19 +14,24 @@ const Graphic = (props) => {
     attributes
   } = props;
 
-  // Put all attributes together
-  if (keepRatio) attributes.preserveAspectRatio = 'xMidYMid meet';
-  if (props.className) attributes.className = props.className;
-
   // Deal with building out the string value for viewBox if an array is passed
   if (viewBox.constructor === Array) {
     if (viewBox.length === 2) {
-      attributes.viewBox = `0 0 ${viewBox[0]} ${viewBox[1]}`
+      attributes.viewBox = `0 0 ${viewBox[0]} ${viewBox[1]}`;
     }
     if (viewBox.length === 4) {
-      attributes.viewBox = `${viewBox[0]} ${viewBox[1]} ${viewBox[2]} ${viewBox[3]}`
+      attributes.viewBox = `${viewBox[0]} ${viewBox[1]} ${viewBox[2]} ${viewBox[3]}`;
     }
+  } else {
+    attributes.viewBox = `0 0 ${viewBox} ${viewBox}`;
   }
+
+  // Put all attributes together
+  if (props.className) attributes.className = `${props.className} ${styles.fluid}`;
+  if (height) attributes.height = height;
+  if (width) attributes.width = width;
+  if (keepRatio) attributes.preserveAspectRatio = 'xMidYMid meet';
+
   const element = display ?
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -43,18 +49,18 @@ Graphic.propTypes = {
     PropTypes.arrayOf(PropTypes.number),
     PropTypes.number
   ]).isRequired,
-  height: PropTypes.number,
-  width: PropTypes.number,
-  display: PropTypes.bool,
-  keepRatio: PropTypes.bool,
+  height:     PropTypes.oneOf([PropTypes.bool, PropTypes.number]),
+  width:      PropTypes.oneOf([PropTypes.bool, PropTypes.number]),
+  display:    PropTypes.bool,
+  keepRatio:  PropTypes.bool,
   attributes: PropTypes.object
 };
 
-Graphic.defaulProps = {
-  height: false,
-  width: false,
-  display: true,
-  keepRatio: true,
+Graphic.defaultProps = {
+  height:     false,
+  width:      false,
+  display:    true,
+  keepRatio:  true,
   attributes: {}
 };
 
